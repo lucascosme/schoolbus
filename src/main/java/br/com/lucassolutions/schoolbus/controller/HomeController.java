1b70bb253c377cbbc792bc90327a262b2defff98
lucascosme <lucas-cosme1@hotmail.com>
2016-07-19 22:02:24 -0300

    Refatorando homeController

--- src/main/java/br/com/lucassolutions/schoolbus/controller/HomeController.java
+++ src/main/java/br/com/lucassolutions/schoolbus/controller/HomeController.java
@@ -1,10 +1,7 @@
 package br.com.lucassolutions.schoolbus.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import br.com.lucassolutions.schoolbus.dao.StudentDao;
 
 
 @Controller
@@ -12,8 +9,6 @@ import br.com.lucassolutions.schoolbus.dao.StudentDao;
 public class HomeController {
 
 	private static final String HOME = "home";
-	
-	@Autowired private StudentDao studentDao;
 	
 	@RequestMapping("/home")
 	public String view() {
